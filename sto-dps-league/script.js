2497c2c89d03c06a49d050f0061bb1d6da977491
ComaToes <comatoes@nonononononono.com>
2014-01-09 12:40:28 +0000

    Added STO DPS league page

---
+++ sto-dps-league/script.js
@@ -0,0 +1,28 @@
+var dpsLeague = angular.module( "dps-league", [] );
+
+function DpsLeagueCtrl($scope, $http) {
+  
+  $scope.dpsData = [];
+  
+  $scope.refresh = function() {
+    
+    $http.get("http://sto-dps-league.herokuapp.com/data")
+    	.success( function(data) {
+        var players = {};
+			  $scope.dpsData = $.map( data, function(item, i) {
+			    if( !players[item.player] ) {
+			      players[item.player] = true;
+			      item.rank = Object.keys(players).length;
+			      return item;
+			    }
+			    return null;
+			  } );
+		  });
+    
+  }
+  
+  $scope.refresh();
+  
+  
+  
+}
